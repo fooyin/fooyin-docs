e35541f775ea99a55f052a8e15bfdf99dc1cadc9
Luke Taylor <LukeT1@proton.me>
2024-09-27 12:48:17 +0100

    Don't collapse navigation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,16 +36,7 @@ html_favicon = 'favicon.ico'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+    'collapse_navigation': False,
 }
 
 # -- Options for EPUB output
