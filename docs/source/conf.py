ec91afd2f899ea314fab2ea3793bb7cbc1cfbfac
Luke Taylor <luket@pm.me>
2026-07-25 20:35:10 +0100

    Remove unused Sphinx extensions and add custom CSS for wider content layout

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,16 +14,7 @@ version = '0.1.0'
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
 ]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
@@ -31,6 +22,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 html_logo = 'logo.svg'
 html_favicon = 'favicon.ico'
 html_theme_options = {
